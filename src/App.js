0f444398fda595985965d3e9e2e25aa7cfff1803
Jason <eric870615@gmail.com>
2021-08-23 18:09:21 +0800

    use React.Fragment instant <div>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
+import React,{Fragment} from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+    render(){
+        return(
+            <Fragment>
+                <h1>Hi This H1 and used React.Fragment</h1>
+                <h2>This H2 use parent React.Fragment</h2>
+            </Fragment>
+        )
+    }
+}
+
+function OldApp() {
   return (
+
     <div className="App">
       <h1>Hello from React !</h1>
+      <h2>This is H2 !</h2>
     </div>
   );
 }
