1e355d14ec07433685b359cb0b43c533a56aec42
Jason <eric870615@gmail.com>
2021-09-08 22:03:05 +0800

    add state to make component state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,16 +3,39 @@ import React, { Component } from 'react'
 class UserItem extends Component {
     constructor(){
         super();
-        this.user = {
-            id : 'Jason',
-            user_url : 'https://github.com/jason1950'
-        }
+        this.state = {
+            id : 'id',
+            login :'jason1950',
+            html_url : 'https://github.com/jason1950',
+            avatar_rul : 'https://avatars.githubusercontent.com/u/45868260?s=60&v=4'
+        };
     }
 
+    stateNotConstructor = {
+        id : 'id',
+        login :'jason1950',
+        html_url : 'https://github.com/jason1950',
+        avatar_rul : 'https://avatars.githubusercontent.com/u/45868260?s=60&v=4'
+    };
+
     render() {
+        const {login, html_url, avatar_rul} = this.stateNotConstructor
         return (
-            <div>
-                UserItem
+            <div className="card text-center">
+                <img 
+                    src={avatar_rul}
+                    alt=''
+                    className="round-img"
+                    style={{width:'60px'}}
+                />
+                <h2>
+                    {login}
+                </h2>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                        More
+                    </a>
+                </div>
             </div>
         );
     }
